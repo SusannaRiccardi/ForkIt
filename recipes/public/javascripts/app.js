e83a1eba5147f302dcdb693f4a7d89d1a8630e07
samuelebischof <samuele.bischof@usi.ch>
2016-11-28 11:18:36 +0100

    Testing html

--- recipes/public/javascripts/app.js
+++ recipes/public/javascripts/app.js
@@ -1,17 +1,16 @@
 window.onload = function() {
-    let receipts = document.getElementsByClassName('receipt-preview');
+    // let receipts = document.getElementsByClassName('receipt-preview');
 
-    for (let i = 0; i < receipts.length; i++) {
-        receipts[i].addEventListener('mouseover', function() {
-            console.log(receipts[i]);
-            receipts[i].getElementsByClassName('receipt-text')[0].setAttribute('style', 'display: none;');
-        });
-    }
-    for (let i = 0; i < receipts.length; i++) {
-        receipts[i].addEventListener('mouseout', function() {
-            console.log(receipts[i]);
-            receipts[i].getElementsByClassName('receipt-text')[0].removeAttribute('style');
-        });
-    }
+    // for (let i = 0; i < receipts.length; i++) {
+    //     receipts[i].addEventListener('mouseover', function() {
+    //         console.log(receipts[i]);
+    //         receipts[i].getElementsByClassName('receipt-text')[0].setAttribute('style', 'display: none;');
+    //     });
+    // }
+    // for (let i = 0; i < receipts.length; i++) {
+    //     receipts[i].addEventListener('mouseout', function() {
+    //         console.log(receipts[i]);
+    //         receipts[i].getElementsByClassName('receipt-text')[0].removeAttribute('style');
+    //     });
+    // }
 }
-
