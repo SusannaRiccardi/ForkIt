ebba15bb014dd425c5817ba98b5f17102f83b519
Susanna Riccardi <susi.96@hotmail.it>
2016-11-29 15:13:04 +0100

    Added recipes route.

--- recipes/routes/index.js
+++ recipes/routes/index.js
@@ -1,22 +1,24 @@
+// This is just something to start. As soon as we have a more complete website, the routes will be adapted.
+
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  // res.render('index', { title: 'Express' });
-  // I have to query the db
-  data = queryDb();
+  // TODO: Deal with index route.
+  res.render('index', { title: 'Express' });
 
-  htmlOrJson(req,res, data, 'recipes')
+  // data = queryDb();
+  // htmlOrJson(req,res, data, 'recipes')
 });
 
 
-function htmlOrJson(req, res, data, viewName){
-  if(req.accepts('html')){
-    res.render(viewName, data)
-  }else{
-    res.json(data)
-  }
-}
+// function htmlOrJson(req, res, data, viewName) {
+//   if(req.accepts('html')){
+//     res.render(viewName, data)
+//   }else{
+//     res.json(data)
+//   }
+// }
 
 module.exports = router;
--- recipes/routes/recipes.js
+++ recipes/routes/recipes.js
@@ -1,7 +1,10 @@
+// This is just something to start. As soon as we have a more complete website, the routes will be adapted.
+
 var express = require('express');
 var router = express.Router();
 const mongoose = require('mongoose');
 const Recipe = mongoose.model('Recipe');
+const fieldsFilter = {'__v' : 0};
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -9,14 +12,65 @@ router.get('/', function(req, res, next) {
     if (err) {
       throw err
     }
-
+    // Add href.
     for (let obj of found) {
-      obj.href = '/recipes/' + obj._id;
+      addLinks(obj);
     }
-    let recipes = {'recipes' : found};
-    res.render('layout', )
 
+    let recipes = {'recipes' : found};
+    htmlOrJson(req, res, recipes, 'recipes');
   })
 });
+
+// Get /recipes/_id -- Get for single recipe view
+router.get('/:recipeid', function(req, res) {
+  Recipe.findByID(req.params.recipe, fieldsFilter).lean().populate('recipe').exec(function(err, recipe) {
+    if (err) {
+      throw err
+    }
+    if (!recipe) {
+      res.status(404);
+      res.json({
+        statusCode: 404,
+        message: "Not Found"
+      });
+      addLinks(recipe);
+      htmlOrJson(req, res, recipe, 'recipes')
+    }
+  })
+})
+
+// Post /recipes
+router.post('/', function(req, res) {
+  // TODO: Take all fields for new recipe.
+  const newRecipe = new Recipe({
+    title: req.body.title,
+    description: req.body.description
+  });
+  newRecipe.save(function(err, saved) {
+    if (err) {
+      throw err
+    }
+    res.json(saved);
+  })
+})
+
+// Function to decide how to render the view.
+function htmlOrJson(req, res, data, viewName) {
+  if (req.accepts('html')) {
+    res.render(viewName, data);
+  } else {
+    res.json(data);
+  }
+}
+
+function addLinks(recipe) {
+  recipe.links = [
+    {
+      "rel" : "self",
+      "href" : config.url + "/recipes/" + recipe._id
+    }
+  ];
+}
 
 module.exports = router;
