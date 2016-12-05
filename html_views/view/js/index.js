fdceedf3b6b1d14350e95ee3675025857e09d5e6
samuelebischof <samuele.bischof@usi.ch>
2016-12-05 01:37:02 +0100

    Views merge
    Merged main view with recipe view
    Now in view it’s possible to navigate between two different pages

--- html_views/view/js/index.js
+++ html_views/view/js/index.js
@@ -1,18 +1,25 @@
 window.onload = function() {
     let menubar = document.getElementById('menubar');
+    let recipesBtn = document.getElementById('recipes-view-btn');
     let loginBtn = document.getElementById('login-btn');
     let loginBackBtn = document.getElementById('login-btn-back');
     let menuwrapper = document.getElementById('menuwrapper');
     let login = document.getElementById('login');
+    let recipeView = document.getElementById('recipe-view');
+    let mainView = document.getElementById('main-view');
 
     loginBtn.addEventListener('click', function() {
-        menuwrapper.setAttribute('Style', 'display: none;');
-        login.removeAttribute('Style');
+        login.removeAttribute('style');
+        menuwrapper.setAttribute('style', 'display: none;');
     })
 
     loginBackBtn.addEventListener('click', function() {
-        console.log('test')
-        login.setAttribute('Style', 'display: none;');
-        menuwrapper.removeAttribute('Style');
+        menuwrapper.removeAttribute('style');
+        login.setAttribute('style', 'display: none;');
     })
+
+    recipesBtn.addEventListener('click', function() {
+        recipeView.removeAttribute('style');
+        mainView.setAttribute('style', 'display: none;');
+    });
 };
