b53514d3acde574946f3e93555940338c8640e49
samuelebischof <samuele.bischof@usi.ch>
2016-12-07 19:25:35 +0100

    Added views to the node project
    Added all pug files to the project.
    Single recipe is not yet finished.

--- html_views/view/js/index.js
+++ html_views/view/js/index.js
@@ -2,10 +2,17 @@ window.onload = function() {
     let pageContent = document.getElementById('page-content');
     pageContent.innerHTML = mainTemplate();
 
-    let btnTdRecipe = document.getElementById('btn-tdRecipe');;
-    let btnCreate = document.getElementById('btn-create');;
-    let btnDiscover = document.getElementById('btn-discover');;
-    let btnAbout = document.getElementById('btn-about');;
+    let btnMain = document.getElementById('btn-about');
+    let btnTdRecipe = document.getElementById('btn-tdRecipe');
+    let btnCreate = document.getElementById('btn-create');
+    let btnDiscover = document.getElementById('btn-discover');
+    let btnAbout = document.getElementById('btn-about');
+
+    let btnSearchSubmit = document.getElementById('search-submit-btn');
+
+    btnMain.addEventListener('click', function() {
+        pageContent.innerHTML = mainTemplate();
+    });
 
     btnTdRecipe.addEventListener('click', function() {
         pageContent.innerHTML = recipeTemplate();
@@ -15,13 +22,43 @@ window.onload = function() {
         pageContent.innerHTML = createTemplate();
     });
 
+
+    let receivedArray = [];
+    let receivedJSONPos = 0;
     btnDiscover.addEventListener('click', function() {
-        pageContent.innerHTML = discoverTemplate();
+        doJSONRequest('GET', '/discover', {}, null, function(req, res) {
+            let receivedJSON = req.body;
+
+            for (let recipe of receivedJSON) {
+                receivedArray.push(recipe);
+            }
+
+            pageContent.innerHTML = discoverTemplate({ recipes: receivedArray.slice(receivedJSONPos, receivedJSONPos + 6) });
+            receivedJSONPos += 6;
+        })
     });
 
-    btnAbout.addEventListener('click', function() {
-        pageContent.innerHTML = "";
+    btnSearchSubmit.addEventListener('click', function(e) {
+        e.preventDefault;
+        let search = document.getElementById('searchField').value;
+        let exclude = document.getElementById('excludeField').value;
+        let filter = {
+            search: search,
+            exclude: exclude
+        };
+        let filterJson = JSON.stringify(filter);
+
+        // doJSONRequest(method, url, headers, data, callback){
+        doJSONRequest('GET', '/discover', {}, filterJson, function(req, res) {
+                // console.log(req);
+                // console.log(res);
+            })
+            // pageContent.innerHTML = discoverTemplate();
     });
+
+    // btnAbout.addEventListener('click', function() {
+    //     pageContent.innerHTML = "";
+    // });
 
 
 
@@ -61,10 +98,100 @@ window.onload = function() {
         mainPageSearchContent.setAttribute('placeholder', 'SEACH FOOD');
     });
 
-    for (input of inputField) {
-        input.addEventListener('click', function() {
-            input.removeAttribute('placeholder');
-        });
-    }
+    // for (input of inputField) {
+    //     input.addEventListener('click', function() {
+    //         input.removeAttribute('placeholder');
+    //     });
+    // }
 
 };
+
+//Error checker and json stringify
+function errorHandler(method, url, headers, data, callback) {
+
+    if (method !== "GET" &&
+        method !== "POST" &&
+        method !== "PUT" &&
+        method !== "DELETE") {
+        throw "Method error"
+    };
+
+    if (url === undefined ||
+        url === '' ||
+        url === null) {
+        throw "Url error";
+    }
+
+
+    if (headers === undefined ||
+        headers === null) {
+        throw "Header error"
+    };
+
+    let dataToJson;
+
+    try {
+        if (data !== undefined &&
+            data !== null) {
+            dataToJson = JSON.stringify(data);
+            JSON.parse(dataToJson);
+        }
+    } catch (e) {
+        throw "Data error";
+    }
+
+    if (typeof callback !== "function")
+        throw "Not a function";
+
+    //Return json string
+    return dataToJson
+}
+
+////////////////////////////////////
+function doJSONRequest(method, url, headers, data, callback) {
+
+    //Check # params
+    if (arguments.length !== 5)
+        throw "Required parameters 5";
+
+    let dataToJson;
+
+    try { dataToJson = errorHandler(method, url, headers, data, callback) } catch (e) {
+        throw e;
+    }
+
+    const req = new XMLHttpRequest();
+
+    req.open(method, url, true);
+
+    req.onreadystatechange = function() {
+        if (req.readyState === 4) {
+            if (req.status === 200)
+                callback(JSON.parse(req.responseText));
+            else if (req.status === 204)
+                callback();
+            else
+                console.log(req.status);
+        }
+    };
+
+    dust.render('contactitem', created, function(err, out) {
+        if (!err) document.getElementById('contactList').innerHTML += out;
+
+        addDeleteListener();
+    });
+
+    req.setRequestHeader("Accept", "application/json");
+
+    if (method === "POST" || method === "PUT") {
+        req.setRequestHeader("Content-Type", "application/json;charset=utf-8")
+    };
+
+    for (let header in headers) {
+        req.setRequestHeader(header, headers[header])
+    };
+
+    req.send(dataToJson);
+}
+
+/* AJAX */
--- recipes/app.js
+++ recipes/app.js
@@ -8,10 +8,11 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var recipes = require('./routes/recipes');
+var create = require('./routes/create');
 
 var app = express();
 
-const mongoose   = require('mongoose');
+const mongoose = require('mongoose');
 mongoose.connect(config.mongoUrl + config.mongoDbName);
 
 require('./models');
@@ -30,6 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/recipes', recipes);
+app.use('/create', create);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
