24657a1b8d08424f60c9f12909700b368ed3ad5c
Vanessa Braglia <braglv@usi.ch>
2016-12-08 20:48:25 +0100

    Added some old feature I accidentally removed

--- recipes/public/javascripts/index.js
+++ recipes/public/javascripts/index.js
@@ -21,6 +21,46 @@ window.onload = function() {
   btnDiscover.addEventListener('click', displayPage);
   btnAbout.addEventListener('click', displayPage);
 
+
+
+
+  let menubar = document.getElementById('menubar');
+  let recipesBtn = document.getElementById('recipes-view-btn');
+  let menuwrapper = document.getElementById('menuwrapper');
+  let login = document.getElementById('login');
+  let recipeView = document.getElementById('recipe-view');
+  let mainView = document.getElementById('main-view');
+  let menuBtnFont = document.getElementsByClassName('btn-img')[0].getElementsByTagName('i')[0];
+  let mainPageSearch = document.getElementById('mainPageSearch');
+  let mainPageSearchContent = document.getElementById('food');
+
+  let searchBtn = document.getElementById('search-btn');
+  let searchBackBtn = document.getElementById('search-btn-back');
+  let search = document.getElementById('search');
+
+  let inputField = document.getElementsByTagName('input');
+  let stars = document.getElementsByClassName('star');
+  let starContainer = document.getElementById('stars');
+
+  console.log(mainPageSearch.getAttribute('placeholder'));
+
+  menuwrapper.addEventListener('mouseenter', function() {
+      menuBtnFont.setAttribute('Style', '-ms-transform: rotate(90deg); -webkit-transform: rotate(90deg); transform: rotate(90 deg);');
+  });
+
+  menuwrapper.addEventListener('mouseleave', function() {
+      menuBtnFont.removeAttribute('Style');
+  });
+
+  mainPageSearchContent.addEventListener('focus', function() {
+      mainPageSearchContent.removeAttribute('placeholder');
+  });
+
+  mainPageSearchContent.addEventListener('blur', function() {
+      mainPageSearchContent.setAttribute('placeholder', 'SEACH FOOD');
+  });
+
+
 }
 
 
@@ -46,9 +86,9 @@ function displayPage(e){
   if (href == 'about'){
     pageContent.innerHTML = "";
   }
-  // if (href == search){
-  //
-  // }
+  if (href == search){
+    search();
+  }
 }
 
 
@@ -145,50 +185,6 @@ function addIngredients(){
 //   });
 //
 //
-//
-//   let menubar = document.getElementById('menubar');
-//   let recipesBtn = document.getElementById('recipes-view-btn');
-//   let menuwrapper = document.getElementById('menuwrapper');
-//   let login = document.getElementById('login');
-//   let recipeView = document.getElementById('recipe-view');
-//   let mainView = document.getElementById('main-view');
-//   let menuBtnFont = document.getElementsByClassName('btn-img')[0].getElementsByTagName('i')[0];
-//   let mainPageSearch = document.getElementById('mainPageSearch');
-//   let mainPageSearchContent = document.getElementById('food');
-//
-//   let searchBtn = document.getElementById('search-btn');
-//   let searchBackBtn = document.getElementById('search-btn-back');
-//   let search = document.getElementById('search');
-//
-//   let inputField = document.getElementsByTagName('input');
-//   let stars = document.getElementsByClassName('star');
-//   let starContainer = document.getElementById('stars');
-//
-//   console.log(mainPageSearch.getAttribute('placeholder'));
-//
-//   menuwrapper.addEventListener('mouseenter', function() {
-//     menuBtnFont.setAttribute('Style', '-ms-transform: rotate(90deg); -webkit-transform: rotate(90deg); transform: rotate(90 deg);');
-//   });
-//
-//   menuwrapper.addEventListener('mouseleave', function() {
-//     menuBtnFont.removeAttribute('Style');
-//   });
-//
-//   mainPageSearchContent.addEventListener('focus', function() {
-//     mainPageSearchContent.removeAttribute('placeholder');
-//   });
-//
-//   mainPageSearchContent.addEventListener('blur', function() {
-//     mainPageSearchContent.setAttribute('placeholder', 'SEACH FOOD');
-//   });
-//
-//   // for (input of inputField) {
-//   //     input.addEventListener('click', function() {
-//   //         input.removeAttribute('placeholder');
-//   //     });
-//   // }
-//
-// };
 //
 
 
